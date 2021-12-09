84b8e12284e94eba6637951b4bd5b265e6827a38
Jason Kapp <mtbkapp@gmail.com>
2021-12-09 07:53:09 -0700

    restart on day08 part 2

--- src/advent_of_code_2021/day08.clj
+++ src/advent_of_code_2021/day08.clj
@@ -5,9 +5,7 @@
             [clojure.set :as sets]
             [clojure.string :as string]
             [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.viewer :as v]
-            [clojure.core.logic :as L]
-            [clojure.core.logic.fd :as FD]))
+            [nextjournal.clerk.viewer :as v]))
 
 
 ; read the input into strings
@@ -26,12 +24,16 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce")
 
 
+(defn str->wire-set
+  [s]
+  (into #{} (map (comp keyword str)) s))
+
 (defn read-input
   [input]
   (map (fn [line]
          (let [[unique display] (string/split line #" \| ")]
-           {:unique (string/split unique #"\s")
-            :display (string/split display #"\s")}))
+           {:unique (map str->wire-set (string/split unique #"\s"))
+            :display (map str->wire-set (string/split display #"\s"))}))
        (string/split-lines input)))
 
 (read-input test-input)
@@ -52,7 +54,9 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 (solve-part1 (read-input test-input))
 (solve-part1 (read-input real-input))
 
-; Make each segment of the display a logic variable 
+
+; ## Part 2
+; 
 ; ```
 ; AAAAA
 ; B   C
@@ -64,80 +68,29 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 ; ```
 
 (def digit->segments
-  {0 [:A :B :C :E :F :G]
-   1 [:C :F]
-   2 [:A :C :D :E :G]
-   3 [:A :C :D :F :G]
-   4 [:B :C :D :F]
-   5 [:A :B :D :F :G]
-   6 [:A :B :D :E :F :G]
-   7 [:A :C :F]
-   8 [:A :B :C :D :E :F :G]
-   9 [:A :B :C :D :F :G]})
+  {0 #{:A :B :C :E :F :G}
+   1 #{:C :F}
+   2 #{:A :C :D :E :G}
+   3 #{:A :C :D :F :G}
+   4 #{:B :C :D :F}
+   5 #{:A :B :D :F :G}
+   6 #{:A :B :D :E :F :G}
+   7 #{:A :C :F}
+   8 #{:A :B :C :D :E :F :G}
+   9 #{:A :B :C :D :F :G}})
 
 
-(def count->potential-digits
+(def wire-count->potential-digits
   (reduce (fn [r [digit segs]]
-            (update r (count segs) conj digit))
+            (update r
+                    (count segs)
+                    (fnil conj #{})
+                    digit))
           {}
           digit->segments))
 
-; A recursive (and probably slow) way to build all the unique ways to order
-; a set of elements.
-;
-; The base cases are:
-;   if the input has no elments -> there are no permutations
-;   if the input has 1 element -> there there is only 1 permutation
-; The recursive case is:
-;   Take each element in the set as the first element of a permutation and
-;   concatenate it to the permutations of the rest of the elements.
-;
-; Example:
-; Input: `#{1 2}`
-;
-; Output:
-; | Element | Permutations of remaining |
-; |---------|---------------------------|
-; |`1`      | `[2]`                     |
-; |`2`      | `[1]`                     |
-; `#{[1 2] [2 1]}` 
-;   
-; Example:
-; Input: `#{1 2 3}`
-;
-; Output:
-; | Element | Permutations of remaining |
-; |---------|---------------------------|
-; |`1`      | `[2 3] [3 2]`             |
-; |`2`      | `[1 3] [3 1]`             |
-; |`3`      | `[2 1] [1 2]`             |
-; ```
-; #{[1 2 3] [1 3 2]
-;   [2 1 3] [2 3 1]
-;   [3 2 1] [3 2 1]}
-; ```
-;   
-(defn permutations
-  [xs]
-  (cond (empty? xs) #{} 
-        (= 1 (count xs)) #{(vec xs)}
-        :else (into #{}
-                    (mapcat
-                      (fn [x]
-                        (map #(cons x %) (permutations (disj xs x)))))
-                    xs)))
 
-(permutations #{})
-(permutations #{'a})
-(permutations #{'a 'b})
-(count (permutations #{'a 'b 'c 'd 'e 'f 'g}))
-
-
-; start with all the possibilities
-;
-; 
-
-(def start
+(def init-segment->potential-wires 
   {:A #{:a :b :c :d :e :f :g}
    :B #{:a :b :c :d :e :f :g}
    :C #{:a :b :c :d :e :f :g}
@@ -146,63 +99,53 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
    :F #{:a :b :c :d :e :f :g}
    :G #{:a :b :c :d :e :f :g}})
 
-; start with the patterns where there can only be one digit
-; 
-#_(require 'clojure.core.logic.fd)
-(clojure.repl/dir clojure.core.logic.fd)
 
-; lets start with "ab" which must be the digit 1
-(count->potential-digits (count "ab"))
-
-; now get the segments that make up a 1
-(digit->segments 1)
-
-; now for each of those update the starting possibilities
-(let [wires (set (map (comp keyword str) "ab"))]
-  (reduce (fn [P seg]
-
-            (update P (keyword seg) sets/intersection wires)
-            )
-          start
-          (digit->segments 1)))
-
-(defn step
-  [P digit wires]
-  (let [kws (set (map (comp keyword str) wires))]
-    (reduce
-      (fn [P' seg]
-        (update P' (keyword seg) sets/intersection kws))
-      P
-      (digit->segments digit))))
+(defn init-pattern->possible-digits
+  [pattern]
+  (zipmap pattern
+          (map #(wire-count->potential-digits (count %)) pattern)))
 
 
-(-> start
-    (step 1 "ab")
-    ;(step 4 "eafb")
-    ;(step 7 "dab")
-    ;(step 8 "acedgfb")
-    )
 
-; eval'd from bottom up.
-{"acedgfb" #{8}
- "cefabd" #{6 9 0}
- "cdfgeb" #{6 9 0}
- "cagedb" #{6 9 0}
- "cdfbe" #{2 3 5} ; #{5} 
- "gcdfa" #{2 3 5} ; #{2 5}, must be 2
- "fbcad" #{2 3 5} ; must be 3, cuz contains same wires as 7
- "eafb" #{4}
- "dab" #{7}
- "ab" #{1}}
+(def test-pattern
+  [#{:a :c :e :d :g :f :b}
+   #{:c :a :g :e :d :b}
+   #{:c :e :f :a :b :d}
+   #{:c :d :f :g :e :b}
+   #{:c :d :f :b :e}
+   #{:g :c :d :f :a}
+   #{:f :b :c :a :d}
+   #{:e :a :f :b}
+   #{:d :a :b}
+   #{:a :b}])
 
 
-; ```
-; AAAAA
-; B   C
-; B   C
-; DDDDD
-; E   F
-; E   F
-; GGGGG
-; ```
+(init-pattern->possible-digits test-pattern)
 
+; why doesn't the "x more..." expansion work anymore...
+{#{:b :a} #{1}
+ #{:b :d :a} #{7}
+ #{:e :b :f :a} #{4}
+ #{:e :c :b :d :f} #{3 2 5}
+ #{:c :b :d :f :a} #{3 2 5}
+ #{:g :c :d :f :a} #{3 2 5}
+ #{:e :g :c :b :d :a} #{0 6 9}
+ #{:e :g :c :b :d :f} #{0 6 9}
+ #{:e :c :b :d :f :a} #{0 6 9}
+ #{:e :g :c :b :d :f :a} #{8}}
+
+
+(defn update-for-known-digit
+  [segment->potential-wires wires digit]
+  (let [segments (digit->segments digit)]
+    (reduce (fn [spw [seg pot-wires]]
+              (assoc spw
+                     seg
+                     (if (contains? segments seg)
+                       (sets/intersection pot-wires wires)
+                       (sets/difference pot-wires wires))))
+            {}
+            segment->potential-wires)))
+
+
+(update-for-known-digit init-segment->potential-wires #{:a :b} 1)
